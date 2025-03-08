208dc7118891817143719a8ade501637bdd7022a
leahxt <60754782+leahxt@users.noreply.github.com>
2025-03-08 14:31:41 -0600

    Still not getting reliable performance from httpx, letting ffmpeg handle the download itself

--- dash2mp4.py
+++ dash2mp4.py
@@ -3,7 +3,6 @@ from typing import cast
 from urllib.parse import SplitResult, urljoin, urlsplit, urlunsplit
 from tempfile import NamedTemporaryFile
 
-from httpx import AsyncClient
 from ffmpeg.asyncio import FFmpeg  # type: ignore
 from starlette.applications import Starlette
 from starlette.config import Config
@@ -40,29 +39,13 @@ async def convert(req: Request) -> Response:
     filename_url = urlunsplit(['', '', filename_parts.path, filename_parts.query, filename_parts.fragment])
     file_url = urljoin(base_url, filename_url)
 
-    with (NamedTemporaryFile(suffix='.mp4') as tmp_input,
-          NamedTemporaryFile(suffix='.txt') as tmp_chapters, 
+    with (NamedTemporaryFile(suffix='.txt') as tmp_chapters, 
           NamedTemporaryFile(suffix='.mp4') as tmp_output):
-        
-        async with AsyncClient() as client:
-            input_request = await client.get(file_url)
-            for _ in range(3):
-                if input_request.is_success:
-                    break
-                else:
-                    print(f'Received status code {input_request.status_code} from {file_url}')
-                    input_request = await client.get(file_url)
-
-            if not input_request.is_success:
-                return PlainTextResponse(f'Failed to download requested file for encoding ({input_request.status_code}):\n\n{(await input_request.aread()).decode('utf8')}', status_code=HTTPStatus.BAD_GATEWAY)
-    
-            tmp_input.write(input_request.content)
-            tmp_input.flush()
 
         tmp_chapters.write(chapters.encode('utf8'))
         tmp_chapters.flush()
 
-        ffmpeg = FFmpeg().option('y').input(tmp_input.name).input(tmp_chapters.name).output(tmp_output.name, options={'vn': None, 'codec:a': 'copy', 'movflags': '+faststart'})  #type: ignore
+        ffmpeg = FFmpeg().option('y').input(file_url).input(tmp_chapters.name).output(tmp_output.name, options={'vn': None, 'codec:a': 'copy', 'movflags': '+faststart'})  #type: ignore
         await ffmpeg.execute(timeout=60)
         output = tmp_output.read()
 
