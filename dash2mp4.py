7e81a0a6e0ecbe263ce98a73046c2e7707b2098a
leahxt <60754782+leahxt@users.noreply.github.com>
2025-03-08 13:45:24 -0600

    Returning error status when HTTP error occurs

--- dash2mp4.py
+++ dash2mp4.py
@@ -54,7 +54,7 @@ async def convert(req: Request) -> Response:
                     input_request = await client.get(file_url)
 
             if not input_request.is_success:
-                return PlainTextResponse(f'Failed to download requested file for encoding ({input_request.status_code}): {(await input_request.aread()).decode('utf8')}')
+                return PlainTextResponse(f'Failed to download requested file for encoding ({input_request.status_code}): {(await input_request.aread()).decode('utf8')}', status_code=HTTPStatus.BAD_GATEWAY)
 
             tmp_input.write(await input_request.aread())
             tmp_input.flush()
